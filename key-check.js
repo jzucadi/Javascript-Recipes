b07d4e76a12a3289e68b5d53de2eb1d259f1dac8
James Zaccardo <jameszaccardo@gmail.com>
2019-07-26 03:44:58 -0400

    determine if certain keys are being pressed

---
+++ key-check.js
@@ -0,0 +1,52 @@
+/*
+determine if certain keys are being pressed utility
+*/
+
+var utils = utils || {};
+
+(function() {
+  // checks if the tab key was pressed
+  function tab(e) {
+    var key = e.keyCode || e.which;
+
+    return key === 9 || key === '9';
+  }
+
+  // checks if the enter key was pressed
+  function enter(e) {
+    var key = e.keyCode || e.which;
+
+    return key === 13 || key === '13';
+  }
+
+  // checks if the escape key was pressed
+  function esc(e) {
+    var key = e.keyCode || e.which;
+
+    return key === 27 || key === '27';
+  }
+
+  // checks if the up key was pressed
+  function up(e) {
+    var key = e.keyCode || e.which;
+
+    return key === 38 || key === '38';
+  }
+
+  // checks if the down key was pressed
+  function down(e) {
+    var key = e.keyCode || e.which;
+
+    return key === 40 || key === '40';
+  }
+
+  var keys = {
+    enter: enter,
+    tab: tab,
+    esc: esc,
+    up: up,
+    down: down
+  };
+
+  utils.keys = keys;
+}());
