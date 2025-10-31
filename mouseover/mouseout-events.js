349ad3372400315ea8e74df0697f5a34718ffbea
James Zaccardo <james.zaccardo@adhocteam.us>
2025-10-31 04:01:48 -0400

    Refactor mouseover/mouseout event functions to arrow syntax

--- mouseover/mouseout-events.js
+++ mouseover/mouseout-events.js
@@ -2,91 +2,76 @@
  * Helper function to add mouseover/mouseout events when hovering over a menu or content panel
  * which then adds/removes a class from an ancestor panel
  */
-function SetupMenuAndContentHoverEvents( menuPanelID, menuHoverClass, contentPanelID, contentHoverClass, ancestorPanelID )
-{
-	var ancestorPanel = $( '#' + ancestorPanelID );
+const setupMenuAndContentHoverEvents = (menuPanelID, menuHoverClass, contentPanelID, contentHoverClass, ancestorPanelID) => {
+	const ancestorPanel = $(`#${ancestorPanelID}`);
 
-	var hoverItemStyles = {};
-	hoverItemStyles[menuPanelID] = menuHoverClass;
-	hoverItemStyles[contentPanelID] = contentHoverClass;
+	const hoverItemStyles = {
+		[menuPanelID]: menuHoverClass,
+		[contentPanelID]: contentHoverClass
+	};
 
-	var _this = this;
-	SetupPanelsHoverEvents( hoverItemStyles, ancestorPanel, function( pPanel )
-	{
+	setupPanelsHoverEvents(hoverItemStyles, ancestorPanel, (pPanel) => {
 		// mouse is hovering over one of the panels in hoverItemStyles. If it is the target container panel, and focus
 		// is still in this context, pull focus to that panel
-		var pContext = $.GetContextPanel();
-		if ( !pContext.BHasKeyFocus() && !pContext.BHasDescendantKeyFocus() && !pContext.BHasClass( 'Loading' ) )
-		{
+		const pContext = $.GetContextPanel();
+		if (!pContext.BHasKeyFocus() && !pContext.BHasDescendantKeyFocus() && !pContext.BHasClass('Loading')) {
 			return;
 		}
 
-		if ( pPanel.id == contentPanelID && !pPanel.BHasKeyFocus() && !pPanel.BHasDescendantKeyFocus() )
-		{
+		if (pPanel.id === contentPanelID && !pPanel.BHasKeyFocus() && !pPanel.BHasDescendantKeyFocus()) {
 			pPanel.SetFocus();
 		}
-	} );												
-}
+	});
+};
 
 
 /**
  * for a panel on mouseover add and remove classes on a parent
  */
-function SetupPanelHoverEvent( panel, ancestorPanel, hoverClass, uniqueClasses, fnMouseOverCallback )
-{
-	panel.SetPanelEvent( 
-		'onmouseover', 
-		function() {
-			ancestorPanel.AddClass( hoverClass ); 
+const setupPanelHoverEvent = (panel, ancestorPanel, hoverClass, uniqueClasses, fnMouseOverCallback) => {
+	panel.SetPanelEvent(
+		'onmouseover',
+		() => {
+			ancestorPanel.AddClass(hoverClass);
 			// now see what classes we need to remove
-			$.Each( uniqueClasses, function( classValue ) 
-			{
-				if ( classValue !=  hoverClass )
-				{
-					ancestorPanel.RemoveClass( classValue ); 				
+			$.Each(uniqueClasses, (classValue) => {
+				if (classValue !== hoverClass) {
+					ancestorPanel.RemoveClass(classValue);
 				}
-			} );
+			});
 
-			if ( fnMouseOverCallback )
-				fnMouseOverCallback( panel );
+			if (fnMouseOverCallback) {
+				fnMouseOverCallback(panel);
+			}
 		}
 	);
-	
-	$.RegisterForUnhandledEvent( 'WindowCursorHidden', function() 
-		{
-			$.Each( uniqueClasses, function( classValue ) 
-			{
-				ancestorPanel.RemoveClass( classValue );
-			}
-		 	);
-		} );
-} 
+
+	$.RegisterForUnhandledEvent('WindowCursorHidden', () => {
+		$.Each(uniqueClasses, (classValue) => {
+			ancestorPanel.RemoveClass(classValue);
+		});
+	});
+};
 
 
-function onlyUnique(value, index, self) 
-{ 
-    return self.indexOf(value) === index;
-}
+/**
+ * Filters array to unique values only
+ */
+const onlyUnique = (value, index, self) => self.indexOf(value) === index;
+
 
 /**
  * Manages a map of panels and classes to use when that panel is hovered (and the other classes are removed)
  */
-function SetupPanelsHoverEvents( hoverPanelStyleMap, ancestorPanel, fnMouseOverCallback )
-{
-	var hoverClasses = [];
-	for( var o in hoverPanelStyleMap ) 
-	{
-	    hoverClasses.push(hoverPanelStyleMap[o]);
-	}
+const setupPanelsHoverEvents = (hoverPanelStyleMap, ancestorPanel, fnMouseOverCallback) => {
+	const hoverClasses = Object.values(hoverPanelStyleMap);
+	const uniqueClasses = [...new Set(hoverClasses)];
 
-	uniqueClasses = hoverClasses.filter( onlyUnique ); 
-	$.Each( uniqueClasses, function( classValue ) 
-	{
-		ancestorPanel.RemoveClass( classValue ); 				
-	} );
-	
-	$.Each( hoverPanelStyleMap, function( value, key ) 
-	{
-		SetupPanelHoverEvent( $( '#' + key ), ancestorPanel, hoverPanelStyleMap[key], uniqueClasses, fnMouseOverCallback );		
-	} );
-}
+	$.Each(uniqueClasses, (classValue) => {
+		ancestorPanel.RemoveClass(classValue);
+	});
+
+	$.Each(hoverPanelStyleMap, (value, key) => {
+		setupPanelHoverEvent($(`#${key}`), ancestorPanel, hoverPanelStyleMap[key], uniqueClasses, fnMouseOverCallback);
+	});
+};
