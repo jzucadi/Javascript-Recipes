1e74c0f51ee4cedf8ad5000e6e7453bf613fff43
James Zaccardo <james.zaccardo@adhocteam.us>
2025-11-03 17:38:23 -0500

    Refactor addMultipleListeners function

--- add-multiple-listeners.js
+++ add-multiple-listeners.js
@@ -1,58 +1,58 @@
+const utils = typeof utils !== "undefined" ? utils : {};
+const WindowObj = typeof Window !== "undefined" ? Window : {};
 
-var utils = utils || {},
-  Window = Window || {};
+(() => {
+  /**
+   * Adds multiple event listeners to an element or window
+   * @param {HTMLElement|Window} element
+   * @param {string[]} eventsArray
+   * @param {Function} handler
+   * @param {boolean} [useCapture=false]
+   * @param {any[]} [handlerArgs=[]]
+   */
+  const addMultipleListeners = (
+    element,
+    eventsArray,
+    handler,
+    useCapture = false,
+    handlerArgs = []
+  ) => {
+    const errors = {
+      element: "First argument must be an HTMLElement or Window object",
+      eventsArray: "Second argument must be an array of strings",
+      handler: "Third argument must be a function",
+      useCapture: "Fourth argument must be a boolean value",
+      handlerArgs: "Fifth argument must be an array",
+    };
 
-(function() {
-  var addMultipleListeners = function(_element, _eventsArray, _handler, _useCapture, _handlerArgs) {
-    var element,
-      eventsArray,
-      handler,
-      handlerArgs = [],
-      useCapture,
-      errors = {
-        element: 'First arguments must be an HTMLElement or Window object',
-        eventsArray: 'Second argument must be an array of strings',
-        handler: 'Third argument must be a function',
-        useCapture: 'Fourth argument must be a boolean value',
-        handlerArgs: 'Fifth argument must be an Array of arguments'
-      };
+    if (
+      !(element instanceof HTMLElement || element instanceof WindowObj)
+    ) {
+      throw new TypeError(errors.element);
+    }
 
-      if(_element instanceof HTMLElement || _element instanceof Window) {
-        element = _element;
-      } else {
-        throw new TypeError(errors.element);
-      }
+    if (!Array.isArray(eventsArray)) {
+      throw new TypeError(errors.eventsArray);
+    }
 
-      if(_eventsArray instanceof Array) {
-        eventsArray = _eventsArray;
-      } else {
-        throw new TypeError(errors.eventsArray);
-      }
+    if (typeof handler !== "function") {
+      throw new TypeError(errors.handler);
+    }
 
-      if(_handlerArgs instanceof Array) {
-        handlerArgs = _handlerArgs;
-      } else if(typeof _handlerArgs !== 'undefined') {
-        throw new TypeError(errors.handlerArgs);
-      }
+    if (typeof useCapture !== "boolean" && typeof useCapture !== "undefined") {
+      throw new TypeError(errors.useCapture);
+    }
 
-      if(_handler instanceof Function) {
-        handler = function(e) {
-          _handler.apply(this, [e].concat(handlerArgs));
-        };
-      } else {
-        throw new TypeError(errors.handler);
-      }
+    if (!Array.isArray(handlerArgs) && typeof handlerArgs !== "undefined") {
+      throw new TypeError(errors.handlerArgs);
+    }
 
-      if(typeof _useCapture === 'boolean') {
-        useCapture = _useCapture;
-      } else if(typeof _useCapture !== 'undefined') {
-        throw new TypeError(errors.useCapture);
-      }
+    const wrappedHandler = (e) => handler(e, ...handlerArgs);
 
-      eventsArray.forEach(function(event) {
-        element.addEventListener(event, handler, useCapture);
-      });
+    eventsArray.forEach((event) => {
+      element.addEventListener(event, wrappedHandler, useCapture);
+    });
   };
 
   utils.addMultipleListeners = addMultipleListeners;
-}());
+})();
