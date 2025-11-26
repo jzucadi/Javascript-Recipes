85fa84a61c653c7ad9f479acfe7fbe1bc3c003ba
James Zaccardo <james.zaccardo@adhocteam.us>
2025-11-25 20:30:50 -0500

    Add TypeScript types to swapClass function
    Refactor swapClass function to use TypeScript types and improve type safety.

--- swap-class.js
+++ swap-class.js
@@ -1,21 +1,38 @@
+// Define the utils interface
+interface Utils {
+  swapClass: typeof swapClass;
+}
+
+// Extend the globalThis interface to include utils
+declare global {
+  var utils: Utils | undefined;
+}
+
 // Create a utils object if it doesn't exist
-const utils = globalThis.utils ?? (globalThis.utils = {});
+const utils: Utils = (globalThis. utils ?? (globalThis.utils = {} as Utils)) as Utils;
+
+/**
+ * Interface for the return object of swapClass
+ */
+interface SwapClassReturn {
+  forClass(addClass: string): HTMLElement;
+}
 
 /**
  * Flip-flops two classes.
- * @param {HTMLElement} el
- * @param {string} removeClass
- * @return {Object}
+ * @param {HTMLElement} el - The HTML element to modify
+ * @param {string} removeClass - The class to remove
+ * @return {SwapClassReturn} Object with forClass method for chaining
  *
  * Usage:
- * utils.swapClass(document.getElementById('some-element'), 'class1').forClass('class2');
+ * utils.swapClass(document.getElementById('some-element')!, 'class1').forClass('class2');
  */
-function swapClass(el, removeClass) {
+function swapClass(el: HTMLElement, removeClass: string): SwapClassReturn {
   el.classList.remove(removeClass);
 
   // Chain forClass as a method
   return {
-    forClass(addClass) {
+    forClass(addClass: string): HTMLElement {
       el.classList.add(addClass);
       // Optional: Return element for chaining DOM ops
       return el;
@@ -24,3 +41,5 @@ function swapClass(el, removeClass) {
 }
 
 utils.swapClass = swapClass;
+
+export { swapClass, utils };
